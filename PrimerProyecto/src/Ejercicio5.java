f4bcf155c48393855dcd2e6347001ce49300c046
David <David@DESKTOP-5A2HH1S>
2022-06-21 14:15:23 +0200

    Borrado uno de los mensajes
    Se ha quedao el mensaje printf y se ha borrado el println

--- PrimerProyecto/src/Ejercicio5.java
+++ PrimerProyecto/src/Ejercicio5.java
@@ -13,7 +13,7 @@ public class Ejercicio5 {
 		double area =PI*Math.pow(radio, 2);
 		double longitud = 2 * PI * radio;
 		
-		System.out.println("El área es: "+ area + " cm \ny la longitud es " + longitud+ " cm");
+		//System.out.println("El área es: "+ area + " cm \ny la longitud es " + longitud+ " cm");
 		System.out.printf("El área es %.2f cm2 y la longitud es %.2f cm", area, longitud );
 	}
 }
